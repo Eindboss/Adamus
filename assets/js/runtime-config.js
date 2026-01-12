ac65bc9e3a151885c7b3c1921ce763f2ea65a0a7
Onno Vliegenthart <ovliegenthart@live.nl>
2026-01-12 20:08:48 +0100

    Remove aardrijkskunde h1 from subjects

--- assets/js/runtime-config.js
+++
@@ -1,5 +0,0 @@
-// Runtime configuration - injected at container startup
-// For local development, you can add your API key here (don't commit!)
-window.__ADAMUS_CONFIG__ = {
-  GEMINI_API_KEY: ""
-};
